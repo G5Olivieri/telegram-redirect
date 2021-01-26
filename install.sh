2aef75438c445d4676bd2f61f815d807868dd1f1
Glayson Olivieri <glayson@vizir.com.br>
2021-01-26 17:35:46 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+apt install cmake build-essential git gperf zlib1g-dev libssl-dev openjdk-8-jdk
+
+git clone https://github.com/tdlib/td
+git clone https://github.com/G5Olivieri/telegram-redirect
+
+mkdir td/jnibuild
+cd td/jnibuild
+cmake -DCMAKE_BUILD_TYPE=Release -DTD_ENABLE_JNI=ON -DCMAKE_INSTALL_PREFIX:PATH=../example/java/td ..
+cmake --build . --target install
+
+cd ../../telegram-redirect/tdjni
+cmake . && cmake --build .
+
+cd ..
+./gradlew build --no-daemon
